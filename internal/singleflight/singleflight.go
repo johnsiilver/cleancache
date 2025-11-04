aeb059c586875f5d1818d69598f8e74a72595fdd
John Doak <johnsiilver@gmail.com>
2025-11-04 12:44:36 -0800

    forgot to save something

--- internal/singleflight/singleflight.go
+++ internal/singleflight/singleflight.go
@@ -1,10 +1,5 @@
-// Copyright 2013 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package singleflight provides a duplicate function call suppression
-// mechanism.
-package singleflight // import "golang.org/x/sync/singleflight"
+// Package singleflight provides a duplicate function call suppression mechanism.
+package singleflight
 
 import (
 	"bytes"
